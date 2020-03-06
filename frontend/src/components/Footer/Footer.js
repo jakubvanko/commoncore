4eda6d9702544e5f50ac7339e879bf69b97574c3
jakubvanko <kubko.vanko@gmail.com>
2020-03-06 16:48:43 +0100

    Fixed links

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -49,10 +49,10 @@ const Footer = () => (
         <LinkContainer>
             <StyledLink href={"https://github.com/jakubvanko/commoncore"} target={"_blank"}
                         rel={"noopener noreferrer"}>GitHub</StyledLink>
-            <StyledLink to={'docs/01_getting_started'}>Documentation</StyledLink>
-            <StyledLink>BetterBeacons</StyledLink>
-            <StyledLink>UltraChest</StyledLink>
-            <StyledLink>License</StyledLink>
+            <StyledLink to={"docs/01_getting_started"}>Documentation</StyledLink>
+            <StyledLink to={"docs/04_betterbeacons"}>BetterBeacons</StyledLink>
+            <StyledLink to={"docs/05_ultrachest"}>UltraChest</StyledLink>
+            <StyledLink to={"https://github.com/jakubvanko/commoncore/blob/master/LICENSE"}>License</StyledLink>
         </LinkContainer>
         <Copyright>
             <div>This work is dedicated to the public domain. Rights for parts of the web design go to their respective
