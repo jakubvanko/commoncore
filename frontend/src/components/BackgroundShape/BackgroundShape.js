61d00edeecb08c21fe8a8bdd02db59cc614b6146
jakubvanko <kubko.vanko@gmail.com>
2020-03-08 15:38:22 +0100

    Reverting a broken fix back

--- frontend/src/components/BackgroundShape/BackgroundShape.js
+++ frontend/src/components/BackgroundShape/BackgroundShape.js
@@ -5,11 +5,13 @@ const Container = styled.svg`
     position: absolute;
     bottom: -1px;
     left: -5px;
-    padding-top: 110vh;
+    width: 105vw;
+    max-width: 100%;
+    padding-top: 100vh;
 `;
 
 const BackgroundShape = () => (
-    <Container xmlns="http://www.w3.org/2000/svg" width="100vw" viewBox="0 0 2306.13 167.65">
+    <Container xmlns="http://www.w3.org/2000/svg" viewBox="0 0 2306.13 167.65">
         <path fill="#1b1f3b" d="M0,108.49S496-2.42,1018,0,2306.13,167.65,2306.13,167.65H0Z"/>
     </Container>
 );
